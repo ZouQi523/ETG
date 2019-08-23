ec17ad6114d65b72196d593c33dc0bfb8475b6d5
zouqi_mac_pro <id27149@hotmail.com>
2019-08-23 17:47:31 +0800

    创建工程

---
+++ ETG/ETG/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ETG
+//
+//  Created by 邹琪 on 2019/8/23.
+//  Copyright © 2019 邹琪. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
